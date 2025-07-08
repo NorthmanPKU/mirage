9dcf7a1b3a5f2afd82914580daff86fcd862da8d
Jianan Ji <jji2@andrew.cmu.edu>
2025-07-08 02:57:39 +0000

    support first task to be multi tasks

--- src/kernel/runtime.cc
+++ src/kernel/runtime.cc
@@ -396,11 +396,22 @@ void register_mugraph(
     }
     // Step 2: create events between operators
     if (pre_op == nullptr) {
-      // Assert that the first op launches a single task
-      assert(tasks.size() == 1);
-      first_tasks.push_back(all_tasks.size());
-      cur_task_map[dim3(0, 0, 0)] = all_tasks.size();
-      all_tasks.push_back(tasks[0]);
+      dim3 bid;
+      for (bid.x = 0; bid.x < bgraph.grid_dim.x; bid.x++) {
+        for (bid.y = 0; bid.y < bgraph.grid_dim.y; bid.y++) {
+          for (bid.z = 0; bid.z < bgraph.grid_dim.z; bid.z++) {
+            cur_task_map[bid] = all_tasks.size();
+            
+            int offset = bid.x * bgraph.grid_dim.y * bgraph.grid_dim.z +
+                         bid.y * bgraph.grid_dim.z + bid.z;
+            
+            if (bid.x == 0 && bid.y == 0 && bid.z == 0) {
+              first_tasks.push_back(all_tasks.size());
+            }
+            all_tasks.push_back(tasks[offset]);
+          }
+        }
+      }
     } else {
       // Step 2.1: analyze dependencies between thread blocks of the two ops
       std::vector<int> producer_partition(mirage::config::MAX_TENSOR_DIMS, 1);
